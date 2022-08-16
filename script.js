24f67e8f29e2c5a0a020bb991e8e0fd26333c363
chsaibabu <ch.saibabu.961@gmail.com>
2022-08-16 21:27:38 +0530

    3 files modified

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 console.log('Hello!');
+const phoneImages = [
+  'images/Black.png',
+  'images/Blue.png',
+  'images/Pink.png',
+  'images/Starlight.png',
+];
+let phoneCont = document.getElementById('phoneshow');
+
+let black = document.getElementById('black');
+
+let blue = document.getElementById('blue');
+
+let pink = document.getElementById('pink');
+
+let starlight = document.getElementById('starlight');
+
+let defaultImgItems = `<img src= "${phoneImages.at(0)}">`;
+let blueImgItems = `<img src= "${phoneImages.at(1)}">`;
+let pinkImgItems = `<img src= "${phoneImages.at(2)}">`;
+let starImgItems = `<img src= "${phoneImages.at(3)}">`;
+
+black.addEventListener('click', function () {
+  phoneCont.innerHTML = defaultImgItems;
+});
+blue.addEventListener('click', function () {
+  phoneCont.innerHTML = blueImgItems;
+});
+
+pink.addEventListener('click', function () {
+  phoneCont.innerHTML = pinkImgItems;
+});
+
+starlight.addEventListener('click', function () {
+  phoneCont.innerHTML = starImgItems;
+});
